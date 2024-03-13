c205f4c629f7ae968d9700afd7025ffe34729bf8
Prabhansh Jain <95516177+prabhanshjn@users.noreply.github.com>
2024-03-13 18:26:16 +0530

    Changes

--- src/components/FormBuilder/FormBuilder.tsx
+++ src/components/FormBuilder/FormBuilder.tsx
@@ -1,12 +1,18 @@
-import { FormField } from "@/models/form";
-import { useForm } from "react-hook-form";
+import { FormField, FormFieldType } from "@/models/form";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormData = {
+  [key: string]: string | number | boolean | string[] | undefined;
+};
+
+type FormSubmitHandler = SubmitHandler<FormData>;
 
 const FormBuilder = ({
   fields,
   onSubmit: onSubmit = () => {},
 }: {
   fields: FormField[];
-  onSubmit?: () => void; // TODO: Define appropriate type
+  onSubmit?: FormSubmitHandler; // TODO: Define appropriate type
 }) => {
   const {
     handleSubmit,
@@ -23,6 +29,61 @@ const FormBuilder = ({
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* TODO: Render form fields as defined in `fields` */}
+      {fields.map((field) => {
+        return (
+          <div key={field.key} className="my-2">
+            <p className="my-1">{field.label}</p>
+            {field.type === FormFieldType.SELECT ? (
+              <select
+                {...register(field.key, { required: field.required })}
+                className="select select-bordered w-full max-w-xs"
+              >
+                <option disabled={true} selected>
+                  {field.placeholder}
+                </option>
+                {field.options?.map((option) => (
+                  <option
+                    selected={option.selected}
+                    disabled={option.disabled}
+                    key={option.value}
+                    value={option.value}
+                  >
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            ) : field.type === FormFieldType.CHECKBOX ? (
+              <input
+                {...register(field.key)}
+                defaultChecked={field.checked}
+                type="checkbox"
+                className="checkbox"
+              />
+            ) : field.type === FormFieldType.RADIO ? (
+              <input type="radio" className="radio" />
+            ) : field.type === FormFieldType.TEXTAREA ? (
+              <textarea
+                {...register(field.key, { required: field.required })}
+                className="textarea textarea-bordered"
+                placeholder="Bio"
+              ></textarea>
+            ) : (
+              <input
+                type={field.type}
+                {...register(field.key, {
+                  required: field.required,
+                })}
+                className="input input-bordered w-full max-w-xs"
+                placeholder={field.placeholder}
+                disabled={field.disabled}
+              />
+            )}
+          </div>
+        );
+      })}
+      <button type="submit" className="btn btn-primary btn-md">
+        Submit
+      </button>
     </form>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,70 @@
 import FormBuilder from "@/components/FormBuilder/FormBuilder";
-import { FormField } from "@/models/form";
+import { FormField, FormFieldType } from "@/models/form";
+import { SubmitHandler } from "react-hook-form";
 
 export default function Home() {
-  const fields: FormField[] = []; // TODO: Define form fields for each of the field types
-  const onSubmit = () => {}; // TODO: Define onSubmit handler to log form values to console
+  type FormData = {
+    [key: string]: string | number | boolean | string[] | undefined;
+  };
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log("Form data:", data);
+  };
+
+  const fields: FormField[] = [
+    {
+      type: FormFieldType.TEXT,
+      label: "Name",
+      key: "name",
+      placeholder: "Enter your name",
+      required: true,
+    },
+    {
+      type: FormFieldType.EMAIL,
+      label: "Email",
+      key: "email",
+      placeholder: "Enter your email",
+      required: true,
+      pattern: "/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/i",
+      errorMessages: {
+        required: "Email is required",
+        pattern: "Invalid email address",
+      },
+    },
+    {
+      type: FormFieldType.SELECT,
+      label: "Country",
+      key: "country",
+      placeholder: "Select your country",
+      options: [
+        { label: "USA", value: "usa" },
+        { label: "Canada", value: "canada" },
+        { label: "UK", value: "uk" },
+      ],
+      required: true,
+    },
+    {
+      type: FormFieldType.TEXTAREA,
+      label: "Message",
+      key: "message",
+      placeholder: "Enter your message",
+      required: true,
+    },
+    {
+      type: FormFieldType.CHECKBOX,
+      label: "Subscribe to newsletter",
+      key: "subscribe",
+      checked: false,
+    },
+  ];
 
   return (
-    <div>
-      <h1>hello world</h1>
-      <FormBuilder fields={fields} onSubmit={onSubmit} />
+    <div className="min-h-screen">
+      <div className="max-w-5xl mx-auto pt-20">
+        <div className="shadow border rounded-xl p-6">
+          <FormBuilder fields={fields} onSubmit={onSubmit} />
+        </div>
+      </div>
     </div>
   );
 }
